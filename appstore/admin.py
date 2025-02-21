9f03ca686bb376fce6ed2358625fa1e1c22c9d15
Mohammad Ghaffary <m.ghaffari662@gmail.com>
2025-02-21 23:46:49 +0330

    feat: add verified, rejected and pending actions

--- appstore/admin.py
+++ appstore/admin.py
@@ -1,4 +1,20 @@
 from django.contrib import admin
-from .models import App
+from .models import App, VerificationStatus
 
-admin.site.register(App)
+def mark_as_verified(modeladmin, request, queryset):
+    queryset.update(verification_status=VerificationStatus.VERIFIED)
+mark_as_verified.short_description = 'Mark selected apps as Verified'
+
+def mark_as_rejected(modeladmin, request, queryset):
+    queryset.update(verification_status=VerificationStatus.REJECTED)
+mark_as_rejected.short_description = 'Mark selected apps as Rejected'
+
+def mark_as_pending(modeladmin, request, queryset):
+    queryset.update(verification_status=VerificationStatus.PENDING)
+mark_as_pending.short_description = 'Mark selected apps as Pending'
+
+class AppAdmin(admin.ModelAdmin):
+    list_display = ['title', 'user', 'verification_status', 'category', 'created_at', 'updated_at']
+    actions = [mark_as_verified, mark_as_rejected, mark_as_pending]
+
+admin.site.register(App, AppAdmin)
